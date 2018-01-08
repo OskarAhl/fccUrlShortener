56fcbda632380fb2c8f6f77a76828976f34d6034
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-01-08 22:33:41 +0800

    add endpoints for redirecting url

--- index.js
+++ index.js
@@ -11,12 +11,21 @@ const   MongoClient         = require('mongodb').MongoClient,
         path                = require('path');
 
 let db;
+const Schema = mongoose.Schema;
+const urlSchema = new Schema({
+    url: String,
+    short_url: String,
+    url_number: Number,
+});
+let Url = mongoose.model('Url', urlSchema);
+
 app.use(express.static( __dirname + '/views'));
 app.use(express.static( __dirname + '/scripts'));
 
-MongoClient.connect('mongodb://localhost:27017/url_parser', (err, database) => {
-    if (err) return console.log(err);
-    db = database;
+mongoose.connect('mongodb://localhost:27017/url_parser', { useMongoClient: true });
+db = mongoose.connection;
+db.on('error', console.error.bind(console,  'db connection error'));
+db.once('open', function () {
     app.listen(port, () => console.log('Server running on: ', port));
 });
 
@@ -24,12 +33,50 @@ app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
 
+app.get('/goto/:urlId', (req, res) => {
+    let dbUrl;
+    Url.findOne({'url_number': req.params.urlId}, (err, dbRes) => {
+        if (err) {
+            console.log('error: ', err);
+            res.send('Something went wrong');
+        }
+        if (dbRes) {
+            dbUrl = dbRes;
+            console.log('success: ', dbRes);
+            res.redirect(dbRes.url);
+        }
+    });
+
+});
+
 app.post('/new/:url', (req, res) => {
-    console.log(req.params);
     if (validUrl.isUri(req.params.url)) {
-        res.send(`url param is ${req.params.url}`);        
+        let randomNr = randomDigit();
+        let newUrl = new Url({
+            url: req.params.url,
+            short_url: 'http://localhost:8000/goto/' + randomNr,
+            url_number: randomNr,
+        });
+        newUrl.save(function(err, success) {
+            if (err) {console.log('Error saving url: ', err); }
+            if (success) {
+                console.log(`saved: ${newUrl}`);
+                res.send(
+                    {
+                        original_url: newUrl.url,
+                        short_url: newUrl.short_url
+                    }
+                )
+                // res.send(`The shortened url is: ${newUrl.short_url}`);        
+            }
+        })
     } else {
         res.send('Invalid Url: Please try again');
     }
-})
+});
+
+// helper functions: 
+function randomDigit() {
+    return Math.floor(Math.random() * 100000);    
+}
 
