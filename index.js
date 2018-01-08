c723bfee50533bb0108d22fae36493dcd96c6d80
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-01-08 17:27:10 +0800

    add js post

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const   express         = require('express'),
         app             = express(),
         port            = 8000,
-        mongoose        = require('mongoose');
+        mongoose        = require('mongoose'),
+        shortId         = require('shortid'),
+        validUrl        = require('valid-url');
 
 const   MongoClient         = require('mongodb').MongoClient,
         assert              = require('assert'),
@@ -10,11 +12,11 @@ const   MongoClient         = require('mongodb').MongoClient,
 
 let db;
 app.use(express.static( __dirname + '/views'));
+app.use(express.static( __dirname + '/scripts'));
 
 MongoClient.connect('mongodb://localhost:27017/url_parser', (err, database) => {
     if (err) return console.log(err);
     db = database;
-    db.collection('url').save({test: 'test'});
     app.listen(port, () => console.log('Server running on: ', port));
 });
 
@@ -22,7 +24,12 @@ app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
 
-app.get('/new/:url', (req, res) => {
-    res.send(`url param is ${req.params.url}`);
+app.post('/new/:url', (req, res) => {
+    console.log(req.params);
+    if (validUrl.isUri(req.params.url)) {
+        res.send(`url param is ${req.params.url}`);        
+    } else {
+        res.send('Invalid Url: Please try again');
+    }
 })
 
